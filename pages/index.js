7f082b7e70a869f0aa08f45244c59d876eb9794b
jeandson <jeandsontb@gmail.com>
2021-09-12 16:30:56 -0300

    Filtrando dados

--- pages/index.js
+++ pages/index.js
@@ -7,7 +7,7 @@ const App = (props) => {
       <div>{props.currentDate}</div>
       {props.repoData.map((data) => {
         return (
-          <div>
+          <div key={data.id}>
             <h3>{data.full_name}</h3>
             <p>Language: {data.language} / Stars: {data.stargazers_count}</p>
           </div>
@@ -20,7 +20,23 @@ const App = (props) => {
 export async function getServerSideProps(context) {
 
   const repository = await fetch('https://api.github.com/users/jeandsontb/repos?sort=update');
-  const repoData = await repository.json();
+  const originalRepoData = await repository.json();
+
+  const dontShowRepoData = ['jeandsontb/reality-card'];
+
+  const filterShowRepoData = (repo) => dontShowRepoData.indexOf(repo.full_name) === -1;
+  const isNotFork = (repo) => !repo.fork;
+  const extractRepoData = (repo) => ({
+    id: repo.id,
+    full_name: repo.full_name,
+    language: repo.language,
+    stargazers_count: repo.stargazers_count
+  })
+
+  const repoData = originalRepoData
+                      .filter(isNotFork)
+                      .filter(filterShowRepoData)
+                      .map(extractRepoData);
 
   return {
     props: {
