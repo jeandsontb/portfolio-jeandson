676be0c12cd4df9a1f439b09d10a5311960c7634
jeandson <jeandsontb@gmail.com>
2021-09-12 16:01:31 -0300

    ajuste pasta .next

--- pages/index.js
+++ pages/index.js
@@ -1,9 +1,33 @@
 import React from 'react';
 
-const App = () => {
+const App = (props) => {
   return (
-    <h1>Bem vindo</h1>
+    <div>
+      <h1>Bem vindo</h1>
+      <div>{props.currentDate}</div>
+      {props.repoData.map((data) => {
+        return (
+          <div>
+            <h3>{data.full_name}</h3>
+            <p>Language: {data.language} / Stars: {data.stargazers_count}</p>
+          </div>
+        );
+      })}
+    </div>
   );
+}
+
+export async function getServerSideProps(context) {
+
+  const repository = await fetch('https://api.github.com/users/jeandsontb/repos?sort=update');
+  const repoData = await repository.json();
+
+  return {
+    props: {
+      currentDate: new Date().toString(),
+      repoData,
+    }
+  }
 }
 
 export default App;
